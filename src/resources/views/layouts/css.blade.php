f9570e6c592ef4b8e9cf8515577d4b401d3f2f49
Jayesh Kulkarni <jayesh.kulkarni92@gmail.com>
2017-03-17 18:24:33 +0530

    basic layout

---
+++ src/resources/views/layouts/css.blade.php
@@ -0,0 +1,14 @@
+ <!-- Bootstrap core CSS     -->
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!--  Light Bootstrap Dashboard core CSS    -->
+    <link href="../../assets/css/slate.bootstrap.min.css" rel="stylesheet"/>
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="../../assets/css/demo.css" rel="stylesheet" />
+
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="../../assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
---
+++ src/resources/views/layouts/js.blade.php
@@ -0,0 +1,17 @@
+<!--   Core JS Files and PerfectScrollbar library inside jquery.ui   -->
+    <script src="../../assets/js/jquery.min.js" type="text/javascript"></script>
+    <script src="../../assets/js/jquery-ui.min.js" type="text/javascript"></script>
+	<script src="../../assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+
+	<!--  Forms Validations Plugin -->
+	<script src="../../assets/js/jquery.validate.min.js"></script>
+
+    <!--  Date Time Picker Plugin is included in this js file -->
+    <script src="../../assets/js/bootstrap-datetimepicker.js"></script>
+
+	<!--  Plugin for DataTables.net  -->
+	<script src="../../assets/js/jquery.datatables.js"></script>
+
+    <script src="../../assets/plugins/ckeditor/ckeditor.js"></script>
+
